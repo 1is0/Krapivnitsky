018957c7ce7349b4991579d80c43a91de44c8a63
activnykonch <62112467+activnykonch@users.noreply.github.com>
2020-05-22 20:08:19 +0300

    isp lab

---
+++ lab4/lab4/Process.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace lab4
+{
+    public class WndSearcher
+    {
+        public delegate bool CallBackPtr(IntPtr hwnd, int lParam);
+
+        public CallBackPtr callBackPtr;
+
+        public class EnumReport
+        {
+            [DllImport("user32.dll")]
+            public static extern int EnumWindows(CallBackPtr callPtr, int lPar);
+
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+            public static bool Show(IntPtr hwnd, int lParam)
+            {
+                StringBuilder Name = new StringBuilder();
+                StringBuilder Decription = new StringBuilder();
+                Name.Capacity = 1024;
+                Decription.Capacity = 1024;
+                GetClassName(hwnd, Name, Name.Capacity);
+                GetWindowText(hwnd, Decription, Decription.Capacity);
+                Console.WriteLine($"Name: {Name}\nProcess Decription: {Decription}\n");
+                return true;
+            }
+        }
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Text;
+using static lab4.WndSearcher;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                Console.Clear();
+                CallBackPtr callBackPtr = new CallBackPtr(EnumReport.Show);
+                EnumReport.EnumWindows(callBackPtr, 0);
+                Console.WriteLine("\n\nPress Enter to refresh results or any button to exit");
+            } while (Console.ReadKey(false).Key==ConsoleKey.Enter);
+        }
+    }
+
+}
