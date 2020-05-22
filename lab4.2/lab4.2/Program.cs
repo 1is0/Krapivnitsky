fb5fb5ac635a193cd2ab4cec9e40cddd6195485e
activnykonch <62112467+activnykonch@users.noreply.github.com>
2020-05-22 20:08:45 +0300

    isp lab

---
+++ lab4.2/lab4.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab4._2
+{
+    class Program
+    {
+        [DllImport("math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int sum(int a, int b);
+        [DllImport("math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int mult(int a, int b);
+        [DllImport("math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int div(int a, int b);
+        [DllImport("math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int sub(int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"a + b = {sum(a, b)}\na - b = {sub(a, b)}\na * b = {mult(a, b)}\na \\ b = {div(a, b)}");
+        }
+    }
+}
